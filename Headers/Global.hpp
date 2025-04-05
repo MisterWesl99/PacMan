fc76d3ef773192696bde52458851e6c77686604a
Markus Wetzel <markus.wetzel03@gmail.com>
2025-04-05 20:18:52 +0200

    Initial commit

---
+++ Headers/Global.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+constexpr unsigned char CELL_SIZE = 16;
+constexpr unsigned char MAP_HEIGHT = 21;
+constexpr unsigned char MAP_WIDTH = 21;
+constexpr unsigned char SCREEN_RESIZE = 2;
+
+constexpr unsigned short FRAME_DURATION = 16667;
---
+++ temp.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <array>
+
+std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH> convert_sketch(const std::array<std::string, MAP)
